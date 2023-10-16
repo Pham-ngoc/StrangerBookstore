20800c30c7a18f4b7d815ac1b981090b58f4d8e0
Pham Ngoc <phamngoc0199@gmail.com>
2023-10-17 03:19:49 +0700

    add by Ngoc

---
+++ StrangerBookstore/src/main/java/com/StrangerBookstore/StrangerBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.StrangerBookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrangerBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrangerBookstoreApplication.class, args);
+	}
+
+}
---
+++ StrangerBookstore/src/test/java/com/StrangerBookstore/StrangerBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StrangerBookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrangerBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
